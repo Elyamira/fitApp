d9119ad4de2618a07953dd3ff1e394e53129d098
Elyamira <ohpancaker@gmail.com>
2020-02-13 18:23:32 +0100

    Java Script

---
+++ script.js
@@ -0,0 +1,10 @@
+// console.log("HI");
+var active = document.getElementById('activ_nav');
+// var active_link=document.getElementsByClassName('active');
+active.onclick=function () {removeClass()};
+    // body...
+function removeClass(){
+   console.log("Work")
+    // active.classList.add("active")
+
+}
